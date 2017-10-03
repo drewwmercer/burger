b62808c8e3b44298d836eba9b8014ce015903750
Drew Mercer <japanexpress@gmail.com>
2017-10-03 13:56:10 -0400

    Adding seed burgers to table

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Deluxe Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
