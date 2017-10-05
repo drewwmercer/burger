c9f96c915d5bf8afcb031142af9c5a926b5661d4
Drew Mercer <japanexpress@gmail.com>
2017-10-04 22:57:07 -0400

    Setting up server file

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var app = express();
+var router = require('./controllers/burgers_controller.js');
+
+app.use(express.static('public'));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(methodOverride('_method'));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use('/', router);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('The app is definitely listening on port ' + port);
+});
